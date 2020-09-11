c9a907a935b87ee7e3b94355fdb5163ca2ac8a04
Mario <medurazn@gmail.com>
2020-09-10 21:16:02 -0500

    first commit
    This is the beginning of the project.

---
+++ valet-parking/src/main/java/valet/ValetParkingApplication.java
@@ -0,0 +1,13 @@
+package valet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValetParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValetParkingApplication.class, args);
+    }
+
+}
---
+++ valet-parking/src/test/java/valet/ValetParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package valet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValetParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
