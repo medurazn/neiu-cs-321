c54a5697acfaf905a44f3e04a1f31e21b1a12b2f
Mario <medurazn@gmail.com>
2020-09-18 07:50:58 -0500

    Homework 3.2

--- valet-parking/src/main/java/valet/web/Vehicle.java
+++ valet-parking/src/main/java/valet/web/Vehicle.java
@@ -20,16 +20,22 @@ public class Vehicle {
     private String color;
 
     @Min(value = 4, message = "Needs to be 4 characters long")
-    @NotNull(message="The year is required")
+    @NotNull(message="The year is required ####")
     private Integer year;
 
-
+    @NotNull(message="The ticket is required")
+    @Size(min = 7, message = "The Ticket must be at least 7 characters long with the format 123-456")
     private String ticket;
+
+    @NotNull(message="The location is required")
+    @Size(min = 1, message = "Location must be at least 5 characters long.")
     private String location;
+
+    @NotNull(message="The plate is required")
+    @Size(min = 1, max = 8, message = "Plate number must be at least 1 character and 8 characters max")
     private String plate;
 
     @NotEmpty(message="You must choose at least one model")
-//    @Max(value = 1)
     private List<String> vehicles;
 
 }
